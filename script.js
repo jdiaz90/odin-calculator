85764ed25bca7a44d63c0cd76f08853e201a5587
Javier Díaz <jdr_1_2_1990@hotmail.com>
2023-02-02 12:27:51 +0100

    Add function to be able to write with keyboard

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const buttons = document.querySelectorAll('input[type="button"][data-number]')
+const buttonNumbers = document.querySelectorAll('input[type="button"].number')
+const buttonOperand = document.querySelectorAll('input[type="button"].operand')
 const display = document.querySelector('#display')
 let temp
 let first
 let second
 
-buttons.forEach(button => {
+buttonNumbers.forEach(button => {
 
     button.addEventListener('click', (e) => {
 
-        display.textContent = `${display.textContent}${e.target.getAttribute('data-number')}`
+        display.textContent = `${display.textContent}${e.target.value}`
 
     })
     
 });
+
+    window.addEventListener('keydown', (e) => {
+
+        const character = getKeyboardKeys().find(element => element === e.key)  
+        if(character)
+            display.textContent = `${display.textContent}${character}`  
+
+    })
 
 let add = (a, b) => a + b
 let subtract = (a, b) => a - b
@@ -20,5 +29,12 @@ let multiply = (a, b) => a * b
 let divide = (a, b) => a / b
 
 function operate(){
-    
+
+}
+
+function getKeyboardKeys(){
+
+    let keys = [...document.querySelectorAll('input[type="button"]')]
+    return keys.map(key => key.value)
+
 }
